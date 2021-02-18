824b293fd79456cea360ff2aff26023a324edec6
Saidur Rahman Riad <reidvai1998@gmail.com>
2021-02-19 02:33:02 +0600

    created: inital About section

---
+++ src/components/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h2>About</h2>
+    <div class="content">
+      <p>Hi, I'm Saidur, a multi-talented devil with over eternity worth of experience.</p>
+      <p>You can also call me a product designer, experience designer, bringer of light, UI designer.</p>
+      <p>I am a tech enthusiast and a learner. I define myself by the work I want to do rather than the works I've done. I am always down for learning new skill or a few spells.</p>
+      <p>Fueled by the screams of peasants in purgatory, I'm always inpiring and more than willing to follow my fascination. Just ideas are not enough to contain me, proper execution and acts are what keeping me alive.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    padding: 0 .5rem;
+  }
+
+  .content {
+    padding: 1rem 0;
+  }
+
+  p {
+    margin-bottom: .3rem;
+  }
+
+  p:first-child {
+    font-weight: 400;
+  }
+</style>
