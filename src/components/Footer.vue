febc35ac5398f4d33a374464ff73e37f9f7ec0aa
Saidur Rahman Riad <reidvai1998@gmail.com>
2021-02-23 01:02:28 +0600

    set: max-width

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <footer>
+      <p>©️ 2021 Saidur Rahman Riad</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
+
+<style scoped>
+  footer {
+    background-color: indigo;
+  }
+
+  footer p {
+    color: aliceblue;
+    text-align: center;
+    font-weight: 200;
+    padding: .5rem 0;
+  }
+</style>
