93ec41fdeb32a83eaa835238b52640d973569b9f
Saidur Rahman Riad <reidvai1998@gmail.com>
2021-02-23 22:43:01 +0600

    updated: animated navbar (hide on scroll)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" :class="{ 'hide-navbar': !showNavbar, 'reposition-top': !onTopY }">
     <nav>
       <ul>
         <li><a href="#skills">Skills</a></li>
@@ -13,7 +13,35 @@
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  data() {
+    return {
+      showNavbar: true,
+      onTopY: true,
+      lastScrollPosition: 0
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeUnmount () {
+    window,removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll () {
+      const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      
+      // for not to override momentum scrolling / refresh on mobile devices
+      if (currentScrollPosition < 0 ) return;
+      
+      // to repositon navbar when at top of the screen
+      this.onTopY = currentScrollPosition <= 40
+
+      // do we need to show navbar?
+      this.showNavbar = currentScrollPosition < this.lastScrollPosition
+      this.lastScrollPosition = currentScrollPosition
+    }
+  }
 }
 </script>
 
@@ -27,10 +55,23 @@ export default {
   }
 
   .container {
+    position: sticky;
+    top: 47.59px;
     padding: .8rem;
     display: flex;
     align-items: center;
     justify-content: space-evenly;
     background-color: whitesmoke;
+    transform: translate3d(0, 0, 0);
+    transition: 0.1s all ease-in-out;
+    z-index: 2;
+  }
+
+  .hide-navbar {
+    transform: translate3d(0, -200%, 0);
+  }
+
+  .reposition-top {
+    top: 0px;
   }
 </style>
