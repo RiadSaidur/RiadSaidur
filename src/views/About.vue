1752f38adaab7fefbbb00de35ed281f54dd73471
Saidur Rahman Riad <reidvai1998@gmail.com>
2021-03-21 13:30:37 +0600

    updated: wrote something professional as About

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,10 +7,10 @@
     <div class="content">
       <img src="@/assets/display-photo.jpg" alt="Saidur Riad">
       <div>
-        <p>Hi, I'm Saidur, a multi-talented devil with over eternity worth of experience.</p>
+        <p>Hi, I'm Saidur, a multi-talented Developer with a passion to find simple solution to complex problems.</p>
         <p>You can also call me a Product Designer, Experience Designer, UI Designer.</p>
         <p>I am a tech enthusiast and a learner. I define myself by the work I want to do rather than the works I've done. I am always eager for learning new skill.</p>
-        <p>Fueled by the screams of peasants in purgatory, I'm always inpiring and more than willing to follow my fascination. Just ideas are not enough to contain me, proper execution and acts are what keeping me alive.</p>
+        <p>Fueled by my passion for technology, I'm always inspiring and more than willing to follow my fascination. Just ideas are not enough to contain me, proper execution and acts are what keeping me alive.</p>
       </div>
     </div>
   </section>
