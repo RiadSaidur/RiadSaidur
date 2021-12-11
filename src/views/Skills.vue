7c419d54c708936bf2e553110a5621a2016e69f4
RiadSaidu <reidvai1998@gmail.com>
2021-12-11 22:40:52 +0600

    updated: classified skills

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -5,8 +5,14 @@
       Skills
     </h2>
     <div class="parent-skills" ref="skills">
-      <div class="child-skills" v-for="(skill, idx) in skillsList" :key="idx">
-        <img :src="getImageURL(skill)" :alt="skill" :title="skill">
+      <div class="child-skills" v-for="(skills, key) in skillsLists" :key="key">
+        <h3>{{ key }}</h3>
+        <div class="sub-skills">
+          <figure v-for="(skill, idx) in skills" :key="idx">
+            <img :src="getImageURL(skill)" :alt="skill" :title="skill">
+            <figcaption>{{ skill }}</figcaption>
+          </figure>
+        </div>
       </div>
     </div>
   </section>
@@ -17,7 +23,39 @@ export default {
   name: "Skills",
   data() {
     return {
+      skillsLists: {
+        web: [
+          'html5',
+          'css3',
+          'javascript',  
+        ],
+        ui: [
+          'bootstrap',
+          'material-ui',
+          'vuetify',
+        ],
+        frontend: [
+          'vue',
+          'nuxt',
+          'react',  
+        ],
+        backend: [
+          'firebase',
+          'node',
+          'django',  
+        ],
+        databases: [
+          'mongo',
+          'postgres',
+        ],
+        tools: [
+          'pwa',
+          'figma',
+          'postman'  
+        ]
+      },
       skillsList: [
+        
         'html5',
         'css3',
         'javascript',
@@ -60,9 +98,8 @@ export default {
 
   .parent-skills {
     display: flex;
-    align-items: center;
+    flex-direction: column;
     justify-content: space-evenly;
-    flex-wrap: wrap;
     max-width: 900px;
     margin: 0 auto;
   }
@@ -71,4 +108,25 @@ export default {
     padding: 1rem .5rem;
     margin: 0 1rem;
   }
+
+  figure {
+    padding: 1.5rem 2rem 1rem;
+  }
+
+  figcaption {
+    color: rgb(237,242,247, .8);
+    font-variant: small-caps;
+  }
+
+  .sub-skills {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+
+  h3 {
+    font-variant: small-caps;
+    color: #3BFFD6;
+  }
+
 </style>
